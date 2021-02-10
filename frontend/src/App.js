042e53a6d3b890770f4e3f9655b4beb64758aa9a
dbacall <dbacall@hotmail.co.uk>
2021-02-10 09:26:48 +0000

    add search bar that calls findProperties function on submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,60 @@
-import {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-function App()
-{
-
+function App() {
   /* This is example of how to fetch data from API */
   const [propertyData, setPropertyData] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  useEffect(() =>
-  {
-    async function fetchData()
-    {
+  useEffect(() => {
+    async function fetchData() {
       // demo request to API (ensure it is running!)
-      const resp = await fetch("/lrProperty/17401");
+      const resp = await fetch('/lrProperty/17401');
       const json = await resp.json();
 
-      if(json.success)
-        setPropertyData(json.lrProperty)
+      // if (json.success) setPropertyData(json.lrProperty);
     }
-    
+
     fetchData();
   }, []);
   /* end example */
 
+  const findProperties = async (e) => {
+    e.preventDefault();
+
+    const resp = await fetch(`/lrProperty/${searchQuery}`);
+    const json = await resp.json();
+    console.log('response', json);
+
+    if (json.success) {
+      setPropertyData(json.lrProperty);
+      setSearchQuery('');
+    }
+  };
+
+  const renderSearchBar = () => {
+    return (
+      <Form inline onSubmit={findProperties}>
+        <Form.Control
+          className="mb-2 mr-sm-2"
+          placeholder="Search by property id"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Button type="submit" className="mb-2">
+          Search
+        </Button>
+      </Form>
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {propertyData ? <strong>{propertyData.street}</strong> : null}
+        <h1 className="title">GetAgent Search</h1>
       </header>
+      <div>{renderSearchBar()}</div>
     </div>
   );
 }
