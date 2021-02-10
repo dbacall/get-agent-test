0ff929979f04069237dcced6d0c6d3d9591e69f7
dbacall <dbacall@hotmail.co.uk>
2021-02-10 10:17:37 +0000

    add route with street param that fetches all properties on a street

--- api/routes.js
+++ api/routes.js
@@ -3,32 +3,47 @@ const router = new Router();
 
 const lrProperty = require('./models/lrProperty.js');
 
-router.param('lrPropertyId', async (id, ctx, next) =>
-{
-	ctx.lrProperty = await new lrProperty({id: id}).fetch({withRelated: ['lrTransactions'], require: false});
+router
+  .param('lrPropertyId', async (id, ctx, next) => {
+    ctx.lrProperty = await new lrProperty({ id: id }).fetch({
+      withRelated: ['lrTransactions'],
+      require: false,
+    });
 
-	if(! ctx.lrProperty)
-	{
-		ctx.status = 404;
-		return ctx.body = {error: true, msg: "LRProperty not found"};
-	}
+    if (!ctx.lrProperty) {
+      ctx.status = 404;
+      return (ctx.body = { error: true, msg: 'LRProperty not found' });
+    }
 
-	return next();
+    return next();
+  })
+  .get('/', async (ctx, next) => {
+    return (ctx.body = "I'm alive!");
+  })
+  .get('/lrProperty/:lrPropertyId', async (ctx, next) => {
+    return (ctx.body = { success: true, lrProperty: ctx.lrProperty.toJSON() });
+  });
 
-})
-.get('/', async (ctx, next) => 
-{
-	return ctx.body = "I'm alive!";
-})
-.get('/lrProperty/:lrPropertyId', async (ctx, next) => 
-{
-	return ctx.body = {success: true, lrProperty: ctx.lrProperty.toJSON()};
-});
+router
+  .param('lrPropertyStreet', async (street, ctx, next) => {
+    ctx.lrProperties = await new lrProperty()
+      .where({ street: street })
+      .fetchAll({ withRelated: ['lrTransactions'], require: false });
 
+    if (!ctx.lrProperties) {
+      ctx.status = 404;
+      return (ctx.body = { error: true, msg: 'LRProperties not found' });
+    }
 
-module.exports = (app) =>
-{
-	app
-	.use(router.routes())
-	.use(router.allowedMethods());
+    return next();
+  })
+  .get('/lrProperty/street/:lrPropertyStreet', async (ctx, next) => {
+    return (ctx.body = {
+      success: true,
+      lrProperty: ctx.lrProperties.toJSON(),
+    });
+  });
+
+module.exports = (app) => {
+  app.use(router.routes()).use(router.allowedMethods());
 };
